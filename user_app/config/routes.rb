767c1cce09beb56c2766a9dba0b3011887b3b3f9
Souvick12 <souvick.mondal@quantuminventions.com>
2022-04-13 23:27:09 +0530

    updated folder structure

---
+++ user_app/config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+  root "users#index"
+
+  #get "/users", to: "users#index"
+
+  #get "/users/:id", to: "users#show"
+
+  resources :users
+
+  get "users/get_user_details/:id" => "users#get_user_details", as: :get_user_details_user
+
+
+end
