34e2001397fa1a693eb294aa85b5c6762a937b34
Souvick12 <souvick.mondal@quantuminventions.com>
2022-04-27 17:51:43 +0530

    Add Turbo Drive and Turbo Stream on Quote Editer app

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,54 @@
+class QuotesController < ApplicationController
+
+  def index
+    @quotes = Quote.ordered
+  end
+
+  def show
+    @quote = Quote.find(params[:id])
+  end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+
+    if @quote.save
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
+        format.turbo_stream
+      end
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @quote = Quote.find(params[:id])
+  end
+
+  def update
+    @quote = Quote.find(params[:id])
+
+    if @quote.update(quote_params)
+      redirect_to quotes_path, notice: "Quote was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @quote = Quote.find(params[:id])
+    @quote.destroy
+    
+    redirect_to quotes_path, notice: "Quote was successfully destroyed."
+  end
+
+  private
+  def quote_params
+    params.require(:quote).permit(:name)
+  end
+  
+end
